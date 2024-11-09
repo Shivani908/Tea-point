b30163060cdbf2d692607a773c9a9159a141f081
Shivani90 <jyotishukla05274@gmail.com>
2024-11-09 23:32:45 +0530

    Enjoy

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+var ul = gsap.timeline()
+
+ul.to(".iccon",{
+    top : 0,
+    duration : 0.7
+    
+})
+
+
+ul.pause()
+
+const upper = document.querySelector(".menu");
+const close = document.querySelector(".cross");
+
+upper.addEventListener("click",function(){
+    ul.play()
+})
+
+
+close.addEventListener("click",function(){
+    ul.reverse()
+})
